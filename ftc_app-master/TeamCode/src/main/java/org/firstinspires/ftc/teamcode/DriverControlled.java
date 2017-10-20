5bb8d7376487b514d3004ceefdb12a3ed6d44ee8
laidlawe18 <laidlawe18@barringtonschools.org>
2017-10-20 17:24:07 -0400

    Added basic drivetrain control

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeRegistrar;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 /**
  * Created by BHS-Lab on 10/13/2017.
@@ -16,6 +17,7 @@ public class DriverControlled extends OpMode {
     DcMotor motorDriveRightFront;
     DcMotor motorDriveLeftBack;
     DcMotor motorDriveRightBack;
+    double power = .5;
 
     @Override
     public void init() {
@@ -23,14 +25,41 @@ public class DriverControlled extends OpMode {
         motorDriveRightFront = hardwareMap.dcMotor.get("motor_drive_right_front");
         motorDriveLeftBack = hardwareMap.dcMotor.get("motor_drive_left_back");
         motorDriveRightBack = hardwareMap.dcMotor.get("motor_drive_right_back");
+        motorDriveRightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorDriveRightBack.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
+        telemetry.addData("y", gamepad1.right_stick_y);
+        telemetry.addData("x", gamepad1.right_stick_x);
+        telemetry.addData("power", power);
 
-        motorDriveLeftFront.setPower(gamepad1.right_stick_y - gamepad1.right_stick_x);
-        motorDriveRightBack.setPower(gamepad1.right_stick_y - gamepad1.right_stick_x);
-        motorDriveLeftBack.setPower(gamepad1.right_stick_x + gamepad1.right_stick_y);
-        motorDriveRightFront.setPower(gamepad1.right_stick_x + gamepad1.right_stick_y);
+        if (gamepad1.y && power < 1) {
+            power += .003;
+        } else if (gamepad1.a && power > 0) {
+            power -= .003;
+        }
+
+        motorDriveLeftFront.setPower(scalePower(gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.right_stick_y, gamepad1.right_stick_y + gamepad1.right_stick_x - gamepad1.left_stick_x, power));
+        motorDriveRightBack.setPower(scalePower(gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.right_stick_y, gamepad1.right_stick_y + gamepad1.right_stick_x + gamepad1.left_stick_x, power));
+        motorDriveLeftBack.setPower(scalePower(gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.right_stick_y, -gamepad1.right_stick_x + gamepad1.right_stick_y - gamepad1.left_stick_x, power));
+        motorDriveRightFront.setPower(scalePower(gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.right_stick_y, -gamepad1.right_stick_x + gamepad1.right_stick_y + gamepad1.left_stick_x, power));
+
+        /*
+        motorDriveLeftFront.setPower(gamepad1.left_stick_y);
+        motorDriveLeftBack.setPower(gamepad1.left_stick_x);
+        motorDriveRightFront.setPower(gamepad1.right_stick_y);
+        motorDriveRightBack.setPower(gamepad1.right_stick_x);
+        */
+    }
+
+    private double scalePower(double leftXStick, double xStick, double yStick, double val, double power) {
+        double mag = Math.abs(xStick) + Math.abs(yStick) + Math.abs(leftXStick);
+        if (mag == 0) {
+            return 0;
+        }
+        return power * (val / mag);
+
     }
 }
