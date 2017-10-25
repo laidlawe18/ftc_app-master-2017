3f39b0ca1ec4aaadef8d9beca71d6c51fdc125a5
laidlawe181 <laidlawe18+c9@barringtonschools.org>
2017-10-25 23:46:46 +0000

    added motors and helper methods

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled.java
@@ -13,20 +13,35 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp(name="DriverControlled")
 public class DriverControlled extends OpMode {
 
+    // Drive motors
     DcMotor motorDriveLeftFront;
     DcMotor motorDriveRightFront;
     DcMotor motorDriveLeftBack;
     DcMotor motorDriveRightBack;
+    DcMotor motorLiftLeft;
+    DcMotor motorLiftRight;
+    
+    // Power multiplier for the drive motors
     double power = .5;
 
     @Override
     public void init() {
+        
+        // Get drive motors from hardware map
         motorDriveLeftFront = hardwareMap.dcMotor.get("motor_drive_left_front");
         motorDriveRightFront = hardwareMap.dcMotor.get("motor_drive_right_front");
         motorDriveLeftBack = hardwareMap.dcMotor.get("motor_drive_left_back");
         motorDriveRightBack = hardwareMap.dcMotor.get("motor_drive_right_back");
+        motorLiftLeft = hardwareMap.dcMotor.get("motor_lift_left")
+        motorLiftRight = hardwareMap.dcMotor.get("motor_lift_right")
+        
+        // Reverse directions of backwards motors
         motorDriveRightFront.setDirection(DcMotorSimple.Direction.REVERSE);
         motorDriveRightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        
+        // Reset motor encoders and set to run using encoders
+        setDriveMotorsMode(DcMotorSimple.RunMode.STOP_AND_RESET_ENCODER)
+        setDriveMotorsMode(DcMotorSimple.RunMode.RUN_USING_ENCODER);
     }
 
     @Override
@@ -40,20 +55,31 @@ public class DriverControlled extends OpMode {
         } else if (gamepad1.a && power > 0) {
             power -= .003;
         }
-
-        motorDriveLeftFront.setPower(scalePower(gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.right_stick_y, gamepad1.right_stick_y + gamepad1.right_stick_x - gamepad1.left_stick_x, power));
+        double lfPower = gamepad1.right_stick_y + gamepad1.right_stick_x - gamepad1.left_stick_x;
+        motorDriveLeftFront.setPower(scalePower(gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.right_stick_y, lfPower, power));
         motorDriveRightBack.setPower(scalePower(gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.right_stick_y, gamepad1.right_stick_y + gamepad1.right_stick_x + gamepad1.left_stick_x, power));
         motorDriveLeftBack.setPower(scalePower(gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.right_stick_y, -gamepad1.right_stick_x + gamepad1.right_stick_y - gamepad1.left_stick_x, power));
         motorDriveRightFront.setPower(scalePower(gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.right_stick_y, -gamepad1.right_stick_x + gamepad1.right_stick_y + gamepad1.left_stick_x, power));
 
         /*
+        * Debugging code for checking which motor and what direction is being influenced by each control
         motorDriveLeftFront.setPower(gamepad1.left_stick_y);
         motorDriveLeftBack.setPower(gamepad1.left_stick_x);
         motorDriveRightFront.setPower(gamepad1.right_stick_y);
         motorDriveRightBack.setPower(gamepad1.right_stick_x);
         */
     }
-
+    
+    
+    /**
+     * Input: 
+     *  leftXStick: left joystick x component
+     *  xStick: right joystick x component
+     *  yStick: right joystick y component
+     *  val: unscaled power value for the motor
+     *  power: global power scale
+     *
+     */
     private double scalePower(double leftXStick, double xStick, double yStick, double val, double power) {
         double mag = Math.abs(xStick) + Math.abs(yStick) + Math.abs(leftXStick);
         if (mag == 0) {
@@ -61,5 +87,12 @@ public class DriverControlled extends OpMode {
         }
         return power * (val / mag);
 
+    }
+    
+    private void setDriveMotorsMode(DcMotorSimple.RunMode mode) {
+        motorDriveLeftFront.setMode(mode);
+        motorDriveRightFront.setMode(mode);
+        motorDriveLeftBack.setMode(mode);
+        motorDriveRightBack.setMode(mode);
     }
 }
